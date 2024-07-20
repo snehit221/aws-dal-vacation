dd9dd897bb31b8b73fb963ec3744002d8daa50a6
Shyamal <sgprajapati24@gmail.com>
2024-07-20 16:40:08 -0300

    chore(ui): toast messages

--- ui/src/pages/Authentication/signup/authForm.tsx
+++ ui/src/pages/Authentication/signup/authForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface AuthFormProps {
@@ -31,7 +31,15 @@ const AuthForm: React.FC<AuthFormProps> = ({
     formState: { errors },
     watch,
   } = useForm<FormData>();
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
+  const PasswordVisible = () => {
+    setShowPassword(!showPassword);
+  };
+  const ConfirmPasswordVisible = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
   return (
     <div className="relative mx-auto w-full max-w-7xl items-center px-5 md:px-12 lg:px-20">
       <div className="md:w-84 mx-auto w-full max-w-xl rounded-3xl border-2 bg-gray-50 p-4 shadow-xl sm:p-4 md:max-w-md md:px-6 md:py-4">
@@ -152,24 +160,33 @@ const AuthForm: React.FC<AuthFormProps> = ({
               <label className="text-12 mb-2 ml-1 mt-4 block font-medium text-slate-800">
                 Password
               </label>
-              <input
-                type="password"
-                placeholder="Password"
-                {...register("password", {
-                  required: "Password is required",
-                  minLength: {
-                    value: 8,
-                    message: "Password must not be less than 8 characters",
-                  },
-                  pattern: {
-                    value:
-                      /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/,
-                    message:
-                      "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character",
-                  },
-                })}
-                className="block w-full appearance-none rounded-xl border-2 border-gray-200 bg-white px-6 py-3 text-black placeholder:text-gray-400 focus:border-green-500 sm:text-sm"
-              />
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  {...register("password", {
+                    required: "Password is required",
+                    minLength: {
+                      value: 8,
+                      message: "Password must not be less than 8 characters",
+                    },
+                    pattern: {
+                      value:
+                        /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/,
+                      message:
+                        "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character",
+                    },
+                  })}
+                  className="block w-full appearance-none rounded-xl border-2 border-gray-200 bg-white px-6 py-3 text-black placeholder:text-gray-400 focus:border-green-500 sm:text-sm"
+                />
+                <button
+                  type="button"
+                  onClick={PasswordVisible}
+                  className="absolute right-3 top-1/2 transform border-transparent -translate-y-1/2 bg-transparent"
+                >
+                  {showPassword ? "Hide" : "Show"}
+                </button>
+              </div>
               {errors.password && (
                 <h2 className="mt-[-2px] text-right text-red-500">
                   {errors.password.message}
@@ -177,10 +194,40 @@ const AuthForm: React.FC<AuthFormProps> = ({
               )}
             </div>
             {isSignup && (
+              <div className="relative mt-4">
+                <label className="text-12 mb-2 ml-1 mt-4 block font-medium text-slate-800">
+                  Confirm password
+                </label>
+                <input
+                  className="block w-full appearance-none rounded-xl border-2 border-gray-200 bg-white px-6 py-3 text-black placeholder:text-gray-400 focus:border-green-500 sm:text-sm"
+                  type={showConfirmPassword ? "text" : "password"}
+                  placeholder="Confirm Password"
+                  {...register("confirmPassword", {
+                    required: "Confirm password is required",
+                    validate: (value) =>
+                      value === watch("password") || "Passwords do not match",
+                  })}
+                />
+                <button
+                  type="button"
+                  onClick={ConfirmPasswordVisible}
+                  className="absolute right-3 top-3/4 transform border-transparent -translate-y-1/2 bg-transparent"
+                >
+                  {showConfirmPassword ? "Hide" : "Show"}
+                </button>
+                {errors.confirmPassword && (
+                  <h2 className="mt-[-2px] text-right text-red-500">
+                    {errors.confirmPassword.message}
+                  </h2>
+                )}
+              </div>
+            )}
+            {/* {isSignup && (
               <div>
                 <label className="text-12 mb-2 ml-1 mt-4 block font-medium text-slate-800">
                   Confirm password
                 </label>
+                div
                 <input
                   className="block w-full appearance-none rounded-xl border-2 border-gray-200 bg-white px-6 py-3 text-black placeholder:text-gray-400 focus:border-green-500 sm:text-sm"
                   type="password"
@@ -197,7 +244,7 @@ const AuthForm: React.FC<AuthFormProps> = ({
                   </h2>
                 )}
               </div>
-            )}
+            )} */}
             {isSignup && (
               <div>
                 <label className="text-12 mb-2 ml-1 mt-4 block font-medium text-slate-800">
--- ui/src/pages/Authentication/signup/signup.tsx
+++ ui/src/pages/Authentication/signup/signup.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import AuthForm from "./authForm";
 import { lambdas } from "../../../lib/constants";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface FormData {
   username: string;
@@ -16,20 +18,59 @@ export const Signup = () => {
   const onSubmit = async (values: FormData) => {
     try {
       const response = await axios.post(lambdas.signUp, values);
-      console.log(response.data);
 
-      localStorage.setItem("username", values.username);
-      localStorage.setItem("email", values.email);
-      localStorage.setItem("firstName", values.firstName);
-      localStorage.setItem("lastName", values.lastName);
+      if (response.status === 200) {
+        toast.success(response.data.message || "Sign up successful");
 
-      navigate("/auth/second-factor");
-    } catch {
-      console.log("Error", Error);
+        localStorage.setItem("username", values.username);
+        localStorage.setItem("email", values.email);
+        localStorage.setItem("firstName", values.firstName);
+        localStorage.setItem("lastName", values.lastName);
+
+        navigate("/auth/second-factor");
+      } else {
+        toast.error(
+          `${response.status} ${response.data.message}` || "Sign up failed"
+        );
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          switch (error.response.status) {
+            case 400:
+              toast.error(error.response.data);
+              break;
+            case 409:
+              toast.error(error.response.data);
+              break;
+            case 500:
+              toast.error(error.response.data);
+              break;
+            default:
+              toast.error(
+                `${error.response.status} ${error.response.statusText}`
+              );
+          }
+        } else if (error.request) {
+          toast.error("No response received from server.");
+        } else {
+          toast.error("Request setup failed.");
+        }
+      } else {
+        console.log("Non-Axios error");
+        toast.error("An unexpected error occurred.");
+      }
     }
+
     console.log(values);
   };
-  return <AuthForm title="REGISTRATION FORM" onSubmit={onSubmit} isSignup />;
+
+  return (
+    <>
+      <ToastContainer />
+      <AuthForm title="REGISTRATION FORM" onSubmit={onSubmit} isSignup />
+    </>
+  );
 };
 
 export default Signup;
